27c04c063aad7616c1970f7076c2598603b32ba3
Wallace Carr <wallacecarr4@gmail.com>
2019-01-14 22:40:30 -0700

    Update Linear_Regression and KNN analytics

---
+++ K-Nearest-Neighbors_POPO.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+r_cols = ['user_id', 'movie_id', 'rating']
+ratings = pd.read_csv('/Users/CommanderCarr/Coding/python/data_science/DataScience-Python3 3/ml-100k/u.data', sep='\t', names=r_cols, usecols=range(3))
+ratings.head()
