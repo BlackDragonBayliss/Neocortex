3e5be136992593bac987a1c5c82f94f9232e7ef7
Wallace Carr <wallacecarr4@gmail.com>
2019-01-16 01:10:09 -0700

    Update Decision_Trees_Random_Forests_POPO

--- Decision_Trees_Random_Forests_POPO.py
+++ Decision_Trees_Random_Forests_POPO.py
@@ -1,10 +1,26 @@
-import numpy as np
+# import numpy as np
 import pandas as pd
-from sklearn import tree
+# from sklearn import tree
 
-input_file = "e:/sundog-consult/udemy/datascience/PastHires.csv"
+import csv
+
+input_file = "/Users/CommanderCarr/Coding/python/data_science/DataScience-Python/PastHires.csv"
 df = pd.read_csv(input_file, header = 0)
-df.head()
+# print(df.head())
+
+# with open("/Users/CommanderCarr/Coding/python/data_science/DataScience-Python/PastHires.csv") as csv_file:
+#     csv_reader = csv.reader(csv_file, delimiter=',')
+#     line_count = 0
+#     for row in csv_reader:
+#         if line_count == 0:
+#             print(f'Column names are {", ".join(row)}')
+#             line_count += 1
+#         else:
+#             print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
+#             line_count += 1
+#     print(f'Processed {line_count} lines.')
+
+
 
 d = {'Y': 1, 'N': 0}
 df['Hired'] = df['Hired'].map(d)
@@ -13,8 +29,8 @@ df['Top-tier school'] = df['Top-tier school'].map(d)
 df['Interned'] = df['Interned'].map(d)
 d = {'BS': 0, 'MS': 1, 'PhD': 2}
 df['Level of Education'] = df['Level of Education'].map(d)
-df.head()
-
-
-features = list(df.columns[:6])
-print(features)
+print(df.head())
+#
+#
+# features = list(df.columns[:6])
+# print(features)
