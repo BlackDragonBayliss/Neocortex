27c53e45c9c0bc58dfc0e0a318079ee650511f1d
Wallace Carr <wallacecarr4@gmail.com>
2019-01-15 23:34:01 -0700

    Update Decision_Trees_Random_Forests_POPO

---
+++ Decision_Trees_Random_Forests_POPO.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+from sklearn import tree
+
+input_file = "e:/sundog-consult/udemy/datascience/PastHires.csv"
+df = pd.read_csv(input_file, header = 0)
+df.head()
+
+d = {'Y': 1, 'N': 0}
+df['Hired'] = df['Hired'].map(d)
+df['Employed?'] = df['Employed?'].map(d)
+df['Top-tier school'] = df['Top-tier school'].map(d)
+df['Interned'] = df['Interned'].map(d)
+d = {'BS': 0, 'MS': 1, 'PhD': 2}
+df['Level of Education'] = df['Level of Education'].map(d)
+df.head()
