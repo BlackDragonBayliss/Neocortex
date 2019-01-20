be3a581fb324b0e64f745968fdf1e29dbfc079f5
Wallace Carr <wallacecarr4@gmail.com>
2019-01-19 23:25:47 -0700

    add convolutional test images

---
+++ Convolutional_Neural_Network_POPO.py
@@ -0,0 +1,23 @@
+import tensorflow
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Conv2D, MaxPooling2D, Flatten
+from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras import backend as K
+
+(mnist_train_images, mnist_train_labels), (mnist_test_images, mnist_test_labels) = mnist.load_data()
+
+
+if K.image_data_format() == 'channels_first':
+    train_images = mnist_train_images.reshape(mnist_train_images.shape[0], 1, 28, 28)
+    test_images = mnist_test_images.reshape(mnist_test_images.shape[0], 1, 28, 28)
+    input_shape = (1, 28, 28)
+else:
+    train_images = mnist_train_images.reshape(mnist_train_images.shape[0], 28, 28, 1)
+    test_images = mnist_test_images.reshape(mnist_test_images.shape[0], 28, 28, 1)
+    input_shape = (28, 28, 1)
+
+train_images = train_images.astype('float32')
+test_images = test_images.astype('float32')
+train_images /= 255
+test_images /= 255
