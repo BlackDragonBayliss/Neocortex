2d90a10a09a750797084fe03fbe554ca3165d0fa
Wallace Carr <wallacecarr4@gmail.com>
2019-01-19 23:27:11 -0700

    add convolutional test images

--- Convolutional_Neural_Network_POPO.py
+++ Convolutional_Neural_Network_POPO.py
@@ -4,6 +4,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Conv2D, MaxPooling2D, Flatten
 from tensorflow.keras.optimizers import RMSprop
 from tensorflow.keras import backend as K
+import matplotlib.pyplot as plt
 
 (mnist_train_images, mnist_train_labels), (mnist_test_images, mnist_test_labels) = mnist.load_data()
 
@@ -21,3 +22,17 @@ train_images = train_images.astype('float32')
 test_images = test_images.astype('float32')
 train_images /= 255
 test_images /= 255
+
+train_labels = tensorflow.keras.utils.to_categorical(mnist_train_labels, 10)
+test_labels = tensorflow.keras.utils.to_categorical(mnist_test_labels, 10)
+
+def display_sample(num):
+    #Print the one-hot array of this sample's label
+    print(train_labels[num])
+    #Print the label converted back to a number
+    label = train_labels[num].argmax(axis=0)
+    #Reshape the 768 values to a 28x28 image
+    image = train_images[num].reshape([28,28])
+    plt.title('Sample: %d  Label: %d' % (num, label))
+    plt.imshow(image, cmap=plt.get_cmap('gray_r'))
+    plt.show()
