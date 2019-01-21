8ce1d6b1cf52ce721fbeecb73e161c32f535208f
Wallace Carr <wallacecarr4@gmail.com>
2019-01-20 21:49:28 -0700

    begin recurrent nueral network

---
+++ Recurrent_Neural_Network_POPO.py
@@ -0,0 +1,5 @@
+
+# Think about how amazing this is.
+# We're going to train an artificial neural network how to "read" movie reviews
+# and guess whether the author liked the movie or not from them!
+
