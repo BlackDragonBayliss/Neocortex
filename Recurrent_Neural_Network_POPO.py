0a03602427109bab0efeb75c9324bf6cd0d0edda
Wallace Carr <wallacecarr4@gmail.com>
2019-01-21 23:22:17 -0700

    begin recurrent nueral network

--- Recurrent_Neural_Network_POPO.py
+++ Recurrent_Neural_Network_POPO.py
@@ -3,3 +3,16 @@
 # We're going to train an artificial neural network how to "read" movie reviews
 # and guess whether the author liked the movie or not from them!
 
+from tensorflow.keras.preprocessing import sequence
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Embedding
+from tensorflow.keras.layers import LSTM
+from tensorflow.keras.datasets import imdb
+
+print('Loading data...')
+(x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=20000)
+
+# x_train[0]
+
+x_train = sequence.pad_sequences(x_train, maxlen=80)
+x_test = sequence.pad_sequences(x_test, maxlen=80)
