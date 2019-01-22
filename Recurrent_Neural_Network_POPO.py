b9fb21a4cfb08760ccf5b58f1291a9735a881a9e
Wallace Carr <wallacecarr4@gmail.com>
2019-01-21 23:22:56 -0700

    add adam optimizer recurrent nueral network

--- Recurrent_Neural_Network_POPO.py
+++ Recurrent_Neural_Network_POPO.py
@@ -16,3 +16,12 @@ print('Loading data...')
 
 x_train = sequence.pad_sequences(x_train, maxlen=80)
 x_test = sequence.pad_sequences(x_test, maxlen=80)
+
+model = Sequential()
+model.add(Embedding(20000, 128))
+model.add(LSTM(128, dropout=0.2, recurrent_dropout=0.2))
+model.add(Dense(1, activation='sigmoid'))
+
+model.compile(loss='binary_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
