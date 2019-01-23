975da33fa71196ed4f39765874daa398baa81a52
Wallace Carr <wallacecarr4@gmail.com>
2019-01-22 23:29:20 -0700

    add adam optimizer recurrent nueral network

--- Recurrent_Neural_Network_POPO.py
+++ Recurrent_Neural_Network_POPO.py
@@ -1,7 +1,6 @@
 
-# Think about how amazing this is.
-# We're going to train an artificial neural network how to "read" movie reviews
-# and guess whether the author liked the movie or not from them!
+# Training an artificial neural network how to "read" movie reviews
+
 
 from tensorflow.keras.preprocessing import sequence
 from tensorflow.keras.models import Sequential
@@ -12,7 +11,6 @@ from tensorflow.keras.datasets import imdb
 print('Loading data...')
 (x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=20000)
 
-# x_train[0]
 
 x_train = sequence.pad_sequences(x_train, maxlen=80)
 x_test = sequence.pad_sequences(x_test, maxlen=80)
